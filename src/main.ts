0b053801b77efc100e929ddae92f10d5c866a3cf
NordicBeaver <nordicbeaver@gmail.com>
2021-10-11 16:37:37 +0300

    Simple orientation tracking.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
 console.log('Hi');
+
+const myConsole = document.getElementById('console');
+const orientationElement = document.getElementById('orientation');
+
+const button = document.getElementById('btn');
+button.addEventListener('click', async () => {
+  try {
+    const permission = await (DeviceOrientationEvent as any).requestPermission();
+    myConsole.innerText = permission;
+  } catch (e) {
+    myConsole.innerText = e;
+  }
+});
+
+window.addEventListener('deviceorientation', (event) => {
+  orientationElement.innerText = `${event.alpha} ${event.beta}, ${event.gamma}`;
+});
